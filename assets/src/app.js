a4f67edb9b45a64d8e12f7ca2ec7c3afaf2e314a
David Parry <david.parry@suranyami.com>
2018-10-16 15:31:05 +1100

    Added @absinthe/socket library for subscriptions over Websockets

--- assets/src/app.js
+++ assets/src/app.js
@@ -5,6 +5,37 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 import VueApollo from 'vue-apollo'
 
+import * as AbsintheSocket from "@absinthe/socket";
+import {Socket as PhoenixSocket} from "phoenix";
+
+const absintheSocket = AbsintheSocket.create(
+  new PhoenixSocket("ws://localhost:4000/socket")
+);
+
+const userAddedSub = `
+  subscription {
+    userAdded {
+      id
+    }
+  }
+`
+
+// This example uses a subscription, but the functionallity is the same for
+// all operation types (queries, mutations and subscriptions)
+
+const notifier = AbsintheSocket.send(absintheSocket, {
+  userAddedSub,
+  variables: {userId: 10}
+})
+
+const logEvent = eventName => (...args) => console.log(eventName, ...args);
+
+const updatedNotifier = AbsintheSocket.observe(absintheSocket, notifier, {
+  onAbort: logEvent("abort"),
+  onError: logEvent("error"),
+  onStart: logEvent("open"),
+  onResult: logEvent("result")
+})
 
 // Import our main app component, the top-level container for our app.
 import App from './App.vue'
